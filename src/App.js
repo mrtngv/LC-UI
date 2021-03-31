425ef6b1f47dfbd530ffdd5081e467c310d51e18
mrtng <mrtng@DESKTOP-5VSJ87A>
2021-03-31 11:01:21 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonClicked: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(previousState => ({
+      buttonClicked: previousState.buttonClicked + 1
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello Mqu Team</h1>
+        <span>
+          <label>Click me</label><br />
+          <button onClick={this.handleClick}>I am a button</button>
+        </span>
+        <h2>{"Clicked: " + this.state.buttonClicked}</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
