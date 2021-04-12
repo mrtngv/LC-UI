04dea886ba28236a6517c9f2edfc715c002357f4
mrtng <mrtng@DESKTOP-5VSJ87A>
2021-04-12 06:58:55 -0700

    added offices sceleton

---
+++ src/OfficesData.js
@@ -0,0 +1,22 @@
+const OfficesData = [
+    {
+        officeId: "1",
+        officeLocation: "Сердика 5",
+        OfficeCity: "София",
+        officeTelephone: "0888412"
+    },
+    {
+        officeId: "2",
+        officeLocation: "Бул. Витоша 91",
+        OfficeCity: "София",
+        officeTelephone: "0888413"
+    },
+    {
+        officeId: "3",
+        officeLocation: "ул. Стъклен свързан списък",
+        OfficeCity: "Бургас",
+        officeTelephone: "0888414"
+    },
+]
+
+export default OfficesData;
