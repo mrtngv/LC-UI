48f4448a543fbecb9f8cf977ab9245a576490eea
Preslava <preslava.petkova@tryzens.com>
2021-05-27 17:46:02 +0300

    Authentication using session storage, profile page, navigation bar

---
+++ src/Profile.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Redirect } from 'react-router-dom';
+import "./Profile.css";
+
+import "@ui5/webcomponents/dist/Card";
+import "@ui5/webcomponents/dist/Label";
+import "@ui5/webcomponents/dist/Title";
+
+class Profile extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    mapUserRoles(user) {
+    return user.roles.map(function(role) {
+        switch (role) {
+        case 'ROLE_CLIENT':
+            return 'Клиент';
+            break;
+        case 'ROLE_OFFICE_EMPLOYEE':
+            return 'Офис служител';
+            break;
+        case 'ROLE_DELIVERY':
+            return 'Доставчик';
+            break;
+        case 'ROLE_MODERATOR':
+            return 'Модератор';
+            break;
+        default:
+            break;
+        }
+    });
+    }
+
+    render() {
+        const user = JSON.parse(sessionStorage.getItem('user'));
+
+        if (!user) {
+            return <Redirect to="/login" />;
+        }
+
+        return (
+            <div className="container">
+            <div className="profile-wrapper">
+                <ui5-card heading={user.username.charAt(0).toUpperCase() + user.username.slice(1)} subheading={this.mapUserRoles(user).map(role => role)} class="small">
+                <div className="content">
+                    <ui5-button icon="activate">Промяна на потрбителски данни</ui5-button>
+                </div>
+                </ui5-card>
+            </div>
+            </div>
+        );
+    }
+}
+
+export default Profile
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import "./Navbar.css";
 
 
@@ -7,7 +6,8 @@ class Navbar extends React.Component {
   constructor(props) {
     super(props);
     this.onNavbarSelect = this.onNavbarSelect.bind(this);
-    
+    this.onLogin = this.onLogin.bind(this);
+    this.onLogout = this.onLogout.bind(this);
 }
 
 onNavbarSelect(event) {
@@ -20,31 +20,51 @@ onSidebarCollapse() {
   sidenav.collapsed = !sidenav.collapsed;
 }
 
+onLogin() {
+  this.props.history.push(('/login'));
+}
+
+onLogout() {
+  sessionStorage.removeItem("user");
+  window.location.reload();
+}
+
 addEventListeners() {
     const navigation = document.getElementById('side-navigation');
     navigation.addEventListener("selection-change", this.onNavbarSelect);
     const onSidebarCollapseButton = document.getElementById('startButton');
     onSidebarCollapseButton.addEventListener('click', this.onSidebarCollapse);
+    const shellbar = document.getElementById('shellbar');
+    const popover = document.getElementById('popover');
+    shellbar.addEventListener("profile-click", function(event) {
+      popover.openBy(event.detail.targetRef);
+    });
+    const profile = document.getElementById("profile");
+    if (profile) {
+      profile.addEventListener("click", () => { this.props.history.push('/profile') });
+    }
 }
 
 componentDidMount() {
-    this.addEventListeners();
+  this.addEventListeners();
 }
 
   render() {
+  const user = JSON.parse(sessionStorage.getItem('user'));
     return (
       <div>
         <ui5-shellbar 
-            class="shellbar"
+            className="shellbar"
+            id="shellbar"
             primary-title="CSCB025"
             secondary-title="Проект логистична компания"
         >
-         <ui5-button icon="menu" slot="startButton" id="startButton"></ui5-button>
-         <ui5-button slot="default">Registrate</ui5-button>
+          <ui5-button icon="menu" slot="startButton" id="startButton"></ui5-button>
+          <ui5-avatar id="nav-profile" initials={user ? user.username.charAt(0).toUpperCase() : ''} icon={user ? '' : "customer"} slot="profile"></ui5-avatar>
+
         </ui5-shellbar>
         <div className="sidenav">
           <ui5-side-navigation id="side-navigation">
-              <ui5-side-navigation-item text="Вход/Регистрация" icon="sap-icon://customer" id="login"></ui5-side-navigation-item>
               <ui5-side-navigation-item text="Всички пратки" icon="sap-icon://product" id="package/all"></ui5-side-navigation-item>
               <ui5-side-navigation-item text="Заяви пратка" icon="sap-icon://product" id="package/ship"></ui5-side-navigation-item>
               <ui5-side-navigation-item text="Проследи пратка" icon="sap-icon://sys-find" id="package/track"></ui5-side-navigation-item>
@@ -52,6 +72,14 @@ componentDidMount() {
               <ui5-side-navigation-item text="За компанията" icon="sap-icon://building" id="company"></ui5-side-navigation-item>
           </ui5-side-navigation>          
         </div>
+        <ui5-popover id="popover" placement-type="Bottom" horizontal-align="Right" show-arrow="false">
+          <div className="popover-content">
+            <ui5-list separators="None" >
+              {user ? <ui5-li icon="settings" id="profile">Профил</ui5-li> : <ui5-li icon="settings" onClick={this.onLogin}>Вход/Регистрация</ui5-li>}
+              {user ? <ui5-li icon="log" onClick={this.onLogout}>Изход</ui5-li> : null}
+            </ui5-list>
+          </div>
+        </ui5-popover>
       </div>
     )
   }
