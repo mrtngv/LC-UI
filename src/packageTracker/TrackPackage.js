5b0abcbf630c9756081ea0059e955603cc446d10
Preslava <preslava.petkova@tryzens.com>
2021-06-30 22:14:34 +0300

    Filter offices, track package edit

--- src/packageTracker/TrackPackage.js
+++ src/packageTracker/TrackPackage.js
@@ -9,6 +9,7 @@ import "@ui5/webcomponents/dist/TableColumn";
 import "@ui5/webcomponents/dist/TableRow";
 import "@ui5/webcomponents/dist/TableCell";
 import "./TrackPackage.css";
+import { mapPackageStatus } from "../constants/MapPackageStatus";
 import TrackPackageRow from './TrackPackageRow';
 import image from '../images/track-order.png';
 
@@ -16,9 +17,11 @@ class TrackPackage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            packageId: '',
+            id: '',
+            privateCode: '',
             isFragile: '',
             ePackageType: '',
+            ePackageStatus: '',
             weight: '',
             fromOffice: '',
             fromAddress: '',
@@ -27,23 +30,28 @@ class TrackPackage extends React.Component {
             senderTelephoneNumber: '',
             toOffice: '',
             toAddress: '',
+            fromCity: '',
+            toCity: '',
             receiverFirstName: '',
             receiverLastName: '',
             receiverTelephoneNumber: '',
             isReturnToOffice: '',
-            dateOfDelivery: '',
             isFirm: '',
-            ePayMethod: ''
+            ePayMethod: '',
+            dateOfDelivery: '',
+            dateOfRequest: '',
+            dateOfSending: ''
         };
 
         this.handleInputValue = this.handleInputValue.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.packageType = this.packageType.bind(this);
+        this.editPackage = this.editPackage.bind(this);
     }
 
     handleInputValue(event) {
         this.setState({
-            packageId: event.target.value
+            privateCode: event.target.value
         });
     }
 
@@ -63,14 +71,16 @@ class TrackPackage extends React.Component {
 
     handleSubmit() {
         const URL = "http://localhost:8080/api/packages/specific";
-        const privateCode = this.state.packageId;
+        const privateCode = this.state.privateCode;
 
         axios.post(URL, {
             code: privateCode
         }).then(response => {
             this.setState({
+                id: response.data.id,
                 isFragile: response.data.isFragile,
                 ePackageType: response.data.ePackageType,
+                ePackageStatus: response.data.ePackageStatus,
                 weight: response.data.weight,
                 fromOffice: response.data.fromOffice,
                 fromAddress: response.data.fromAddress,
@@ -83,9 +93,13 @@ class TrackPackage extends React.Component {
                 receiverLastName: response.data.receiverLastName,
                 receiverTelephoneNumber: response.data.receiverTelephoneNumber,
                 isReturnToOffice: response.data.isReturnToOffice,
-                dateOfDelivery: response.data.dateOfDelivery,
                 isFirm: response.data.isFirm,
-                ePayMethod: response.data.ePayMethod
+                ePayMethod: response.data.ePayMethod,
+                fromCity: response.data.fromCity,
+                toCity: response.data.toCity,
+                dateOfRequest: response.data.dateOfRequest,
+                dateOfSending: response.data.dateOfSending,
+                dateOfDelivery: response.data.dateOfDelivery
             });
             document.getElementById('package-info').style.display = "block";
         }).catch(error => {
@@ -111,11 +125,18 @@ class TrackPackage extends React.Component {
         }
     }
 
+    editPackage() {
+        this.props.history.push({
+            pathname: '/package/edit',
+            state: this.state.id
+        });
+    }
+
     render() {
         const user = JSON.parse(sessionStorage.getItem('user'));
         const editPackage = () => {
            if (user && user.role !== "NO_ROLE" && user.role !== "ROLE_CLIENT") {
-                return <ui5-button id="edit-package" data-layout-span="XL9" design="Emphasized" >Редактирай пратка</ui5-button>
+                return <ui5-button id="edit-package" onClick={this.editPackage} data-layout-span="XL9" design="Emphasized" >Редактирай пратка</ui5-button>
            } 
         }
         return (
@@ -136,16 +157,26 @@ class TrackPackage extends React.Component {
                         </div>
 
                         <ui5-table class="package-table" id="table">
-                            <TrackPackageRow title="Номер на пратка" value={this.state.packageId} />
+                            <TrackPackageRow title="Номер на пратка" value={this.state.privateCode} />
+
+                            <ui5-table-row>
+                                <ui5-table-cell slot="default-1" first-in-row>Статус на пратка</ui5-table-cell>
+                                <ui5-table-cell slot="default-2"><ui5-badge color-scheme="6">
+                                <ui5-icon name="accept" slot="icon"></ui5-icon>{mapPackageStatus(this.state.ePackageStatus)}
+                            </ui5-badge></ui5-table-cell>
+                            </ui5-table-row>
+                            <TrackPackageRow title="Дата на заявяване" value={this.state.dateOfRequest.slice(0, 10)} />
+                            <TrackPackageRow title="Дата на изпращане" value={this.state.dateOfSending || '-'} />
+                            <TrackPackageRow title="Дата на получаване" value={this.state.dateOfDelivery || '-'} />
                             <TrackPackageRow title="Тип на пратката" value={this.packageType()} />
                             <TrackPackageRow title="Тегло" value={this.state.weight} />
                             <TrackPackageRow title="Чупливо" value={this.state.isFragile ? 'Да' : 'Не'} />
                             <TrackPackageRow title="Подател" value={this.state.senderFirstName + ' ' + this.state.senderLastName} />
                             <TrackPackageRow title="Телефон на подател" value={this.state.senderTelephoneNumber} />
                             <TrackPackageRow title="Получател" value={this.state.receiverFirstName + ' ' + this.state.receiverLastName} />
-                            <TrackPackageRow title={this.state.fromOffice ? "Изпратено от офис" : "Изпратено от адрес"} value={this.state.fromAddress} />
-                            <TrackPackageRow title={this.state.toOffice ? "Изпратено към офис" : "Изпратено към адрес"} value={this.state.toAddress} />
                             <TrackPackageRow title="Телефон на получател" value={this.state.receiverTelephoneNumber} />
+                            <TrackPackageRow title={this.state.fromOffice ? "Изпратено от офис" : "Изпратено от адрес"} value={this.state.fromCity + ', ' + this.state.fromAddress} />
+                            <TrackPackageRow title={this.state.toOffice ? "Изпратено към офис" : "Изпратено към адрес"} value={this.state.toCity + ', ' + this.state.toAddress} />
                         </ui5-table>
                     </div>
                 </div>
